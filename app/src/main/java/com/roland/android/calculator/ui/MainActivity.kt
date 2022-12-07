373536ee2276bac9c101068a35009738138dabe6
Roland <rolandnnamodi@gmail.com>
2022-12-07 18:37:18 +0100

    Added app icon and launch screen

--- app/src/main/java/com/roland/android/calculator/ui/MainActivity.kt
+++ app/src/main/java/com/roland/android/calculator/ui/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Calculator)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setTheme()
